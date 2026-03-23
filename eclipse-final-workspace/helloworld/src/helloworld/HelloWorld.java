54b57e9a45d833aa2297a5642ff4fe2a4f514105
aarn-m <aaronjamesmoleta@gmail.com>
2026-03-23 22:53:20 +0800

    Reapply "Merge pull request #3 from aarn-m/aaron"
    This reverts commit 09d3ac4e42b99c18dc18bf622cb292a981d6a736.

--- eclipse-final-workspace/helloworld/src/helloworld/HelloWorld.java
+++ eclipse-final-workspace/helloworld/src/helloworld/HelloWorld.java
@@ -6,10 +6,16 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO NOW.");
 		System.out.println("Hello to you too.");
+
 		
 		
 		System.out.println("hello from the alt aaron branch");
 		System.out.println("Hello from the aaron branch.");
+		
+		System.out.println(add(5, 10));
 	}
 
+	static int add(int x, int y) {
+		return x + y;
+	}
 }
