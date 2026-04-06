a3a3cad99625c114b3f644047c1e2a5390ac62af
aarn-m <aaronjamesmoleta@gmail.com>
2026-04-06 22:35:50 +0800

    gui draft

--- eclipse-final-workspace/morseCodeProject/src/morse/MainFrame.java
+++ eclipse-final-workspace/morseCodeProject/src/morse/MainFrame.java
@@ -5,21 +5,34 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JTextArea;
+import javax.swing.JComboBox;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import javax.swing.JButton;
-import javax.swing.JTextPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import java.awt.GridLayout;
+import javax.swing.JTabbedPane;
 
 public class MainFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
+	/**
+	 * @wbp.nonvisual location=-95,348
+	 */
+	private final JComboBox comboBox = new JComboBox();
 
 	/**
 	 * Launch the application.
@@ -41,33 +54,106 @@ public class MainFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public MainFrame() {
-		setTitle("MainFrame");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 750, 300);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setSize(500, 500);
+		setMinimumSize(new Dimension(500, 500));
+		setLocationRelativeTo(null);
 		
-		JLabel lblMorseTranslation = new JLabel("Translation");
-		lblMorseTranslation.setBounds(10, 98, 296, 18);
-		contentPane.add(lblMorseTranslation);
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
 		
-		textField = new JTextField();
-		textField.setBounds(10, 22, 716, 36);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		JPanel MainMenu = new JPanel();
+		tabbedPane.addTab("Main Menu", null, MainMenu, null);
+		MainMenu.setLayout(new BorderLayout(0, 0));
 		
-		JTextPane txtpnHelloWorld = new JTextPane();
-		txtpnHelloWorld.setEditable(false);
-		txtpnHelloWorld.setText("Translation will appear here.");
-		txtpnHelloWorld.setBounds(10, 120, 716, 133);
-		contentPane.add(txtpnHelloWorld);
-
-		JButton btnNewButton_1 = new JButton("Copy");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		JPanel WMC = new JPanel();
+		tabbedPane.addTab("What's Morse Code?", null, WMC, null);
+		WMC.setLayout(new BorderLayout(0, 0));
+		
+		JPanel TranslatorPanel = new JPanel();
+		tabbedPane.addTab("Translator", null, TranslatorPanel, null);
+		TranslatorPanel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		TranslatorPanel.add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblInput = new JLabel("Input");
+		GridBagConstraints gbc_lblInput = new GridBagConstraints();
+		gbc_lblInput.insets = new Insets(0, 0, 5, 0);
+		gbc_lblInput.gridx = 0;
+		gbc_lblInput.gridy = 0;
+		panel.add(lblInput, gbc_lblInput);
+		
+		JScrollPane scrollPane_Input = new JScrollPane();
+		GridBagConstraints gbc_scrollPane_Input = new GridBagConstraints();
+		gbc_scrollPane_Input.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane_Input.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane_Input.gridx = 0;
+		gbc_scrollPane_Input.gridy = 1;
+		panel.add(scrollPane_Input, gbc_scrollPane_Input);
+		
+		JTextArea textArea_Input = new JTextArea();
+		textArea_Input.setLineWrap(true);
+		scrollPane_Input.setViewportView(textArea_Input);
+		
+		JPanel panelButtons = new JPanel();
+		GridBagConstraints gbc_panelButtons = new GridBagConstraints();
+		gbc_panelButtons.insets = new Insets(0, 0, 5, 0);
+		gbc_panelButtons.gridx = 0;
+		gbc_panelButtons.gridy = 2;
+		panel.add(panelButtons, gbc_panelButtons);
+		panelButtons.setLayout(new GridLayout(0, 3, 10, 0));
+		
+		JLabel lblOutput = new JLabel("Output");
+		GridBagConstraints gbc_lblOutput = new GridBagConstraints();
+		gbc_lblOutput.insets = new Insets(0, 0, 5, 0);
+		gbc_lblOutput.gridx = 0;
+		gbc_lblOutput.gridy = 3;
+		panel.add(lblOutput, gbc_lblOutput);
+		
+		JScrollPane panel_GridBagLayout = new JScrollPane();
+		GridBagConstraints gbc_panel_GridBagLayout = new GridBagConstraints();
+		gbc_panel_GridBagLayout.fill = GridBagConstraints.BOTH;
+		gbc_panel_GridBagLayout.gridx = 0;
+		gbc_panel_GridBagLayout.gridy = 4;
+		panel.add(panel_GridBagLayout, gbc_panel_GridBagLayout);
+		
+		JTextArea textArea_Output = new JTextArea();
+		textArea_Output.setEditable(false);
+		textArea_Output.setLineWrap(true);
+		panel_GridBagLayout.setViewportView(textArea_Output);
+		
+		JButton btnTtM = new JButton("Text to MorseCode");
+		btnTtM.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-		        String text = txtpnHelloWorld.getText(); // get text from JTextPane
+				textArea_Output.setText(Translator.textToMorse(textArea_Input.getText()));
+			}
+		});
+		panelButtons.add(btnTtM);
+		
+		JButton btnMtT = new JButton("MorseCode to Text");
+		btnMtT.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea_Output.setText(Translator.morseToText(textArea_Input.getText()));
+			}
+		});
+		panelButtons.add(btnMtT);
+		
+		JButton btnCtC = new JButton("Copy to Clipboard");
+		btnCtC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {	
+		        String text = textArea_Output.getText(); // get text from JTextPane
 		        
 		        StringSelection selection = new StringSelection(text);
 		        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
@@ -75,26 +161,12 @@ public class MainFrame extends JFrame {
 		        JOptionPane.showMessageDialog(null, "Copied to clipboard!");
 			}
 		});
-		btnNewButton_1.setBounds(330, 68, 84, 20);
-		contentPane.add(btnNewButton_1);
+		panelButtons.add(btnCtC);
 		
-		JButton btnNewButton = new JButton("Text to Morse code");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				txtpnHelloWorld.setText(Translator.textToMorse(textField.getText()));
-			}
-		});
-		btnNewButton.setBounds(10, 68, 150, 20);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_2 = new JButton("Morse code to Text");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {	
-				txtpnHelloWorld.setText(Translator.morseToText(textField.getText()));
-			}
-		});
-		btnNewButton_2.setBounds(170, 68, 150, 20);
-		contentPane.add(btnNewButton_2);
-		
+		JPanel Minigame = new JPanel();
+		tabbedPane.addTab("Minigame", null, Minigame, null);
+		Minigame.setLayout(new BorderLayout(0, 0));
+
 	}
+
 }
